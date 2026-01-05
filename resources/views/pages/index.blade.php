71c8da114438943de44ae3d6c3784fd2e2937b4d
Murad Gafaroff <muradgafarof@gmail.com>
2026-01-06 03:43:22 +0400

    get started

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,7 +15,7 @@
             <h1 data-aos="fade-up">Biznesinizi inkişaf etdirmək üçün müasir həllər təklif edirik</h1>
             <p data-aos="fade-up" data-aos-delay="100">Biz veb saytlar hazırlayan istedadlı dizaynerlərdən ibarət komandayıq</p>
             <div class="d-flex flex-column flex-md-row" data-aos="fade-up" data-aos-delay="200">
-              <a href="#about" class="btn-get-started">Get Started <i class="bi bi-arrow-right"></i></a>
+              <a href="{{ route('about') }}" class="btn-get-started">Get Started <i class="bi bi-arrow-right"></i></a>
               <a href="https://www.youtube.com/shorts/ATYNktqXB_k" class="glightbox btn-watch-video d-flex align-items-center justify-content-center ms-0 ms-md-4 mt-4 mt-md-0"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
             </div>
           </div>
